dfc5ffef1fac01c6bc253ae38c2711acb447453d
Marto-kun <martinvozme456@gmail.com>
2025-11-28 11:43:37 -0500

    Creacion del repositorio

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct 
+{
+    int ID;
+    char titulo[100];
+    char autor[50];
+    int pub;
+    char estado[10];
+
+}Libro;
+
+
+
+int main (int argc, char *argv[]) {
+    return 0;
+}
